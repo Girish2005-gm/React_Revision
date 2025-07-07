8e90496be28268c9b4eed47222a3a62db08228ea
Girish2005-gm <gmaheshwari799@gmail.com>
2025-07-07 13:45:40 +0530

    logo name change

--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
     }
     return (
         <div className='flex flex-row justify-between h-16 items-center  shadow-md w-full'>
-            <h2 className='m-9 bg-indigo-200 py-3 px-10 rounded-md text-lg font-bold'>JinuStore</h2>
+            <h2 className='m-9 bg-indigo-200 py-3 px-10 rounded-md text-lg font-bold'>Store</h2>
             <div className='flex mx-3 items-center justify-center'>
                 <ul className='flex gap-3 mx-7'>
                     <Link to={"/"}><li>Home</li></Link>
